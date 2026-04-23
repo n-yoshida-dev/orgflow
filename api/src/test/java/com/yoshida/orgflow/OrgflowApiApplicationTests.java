1ad4cb9aca1736ed48dea6ac660f40b9483b90d2
n-yoshida-dev <annin.for.dev@gmail.com>
2026-04-23 21:50:19 +0900

    initial: APIフォルダを新規追加

---
+++ api/src/test/java/com/yoshida/orgflow/OrgflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoshida.orgflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
