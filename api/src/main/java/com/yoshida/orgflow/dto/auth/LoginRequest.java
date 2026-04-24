4b3d6ff45e53f8b4b9b05a36323eecf85f1f40ee
n-yoshida-dev <annin.for.dev@gmail.com>
2026-04-24 23:30:51 +0900

    mod(api_dto): login_idをキャメルケースに変更

--- api/src/main/java/com/yoshida/orgflow/dto/auth/LoginRequest.java
+++ api/src/main/java/com/yoshida/orgflow/dto/auth/LoginRequest.java
@@ -3,7 +3,7 @@ package com.yoshida.orgflow.dto.auth;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-    @NotBlank String login_id,
+    @NotBlank String loginId,
     @NotBlank String password) {
 
 }
