4c092bc817eb7328bbd66bcaba932ec3126580c6
n-yoshida-dev <annin.for.dev@gmail.com>
2026-05-05 23:41:18 +0900

    feat(repository): Userrepositoryを追加

--- api/src/main/java/com/yoshida/orgflow/repository/UserRepository.java
+++ api/src/main/java/com/yoshida/orgflow/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package com.yoshida.orgflow.repository;
 
-public class UserRepository {
+import java.util.Optional;
+import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yoshida.orgflow.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByLoginId(String loginId);
 }
