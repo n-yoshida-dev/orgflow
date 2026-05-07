10b1bd2b737ccdad3cb1bdcf5c3cc27638241bc9
n-yoshida-dev <annin.for.dev@gmail.com>
2026-05-07 23:27:33 +0900

    feat(service): AuthServiceにuserOptionalによる処理分岐を追加

--- api/src/main/java/com/yoshida/orgflow/service/AuthService.java
+++ api/src/main/java/com/yoshida/orgflow/service/AuthService.java
@@ -1,14 +1,28 @@
 package com.yoshida.orgflow.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.yoshida.orgflow.common.exception.AuthenticationFailedException;
 import com.yoshida.orgflow.dto.auth.LoginResponse;
+import com.yoshida.orgflow.entity.User;
+import com.yoshida.orgflow.repository.UserRepository;
 
 @Service
 public class AuthService {
 
-  public LoginResponse login(String loginId, String password) {
-    return new LoginResponse("dummy-token", null);
+  private final UserRepository userRepository;
+
+  public AuthService(UserRepository userRepository) {
+    this.userRepository = userRepository;
   }
 
+  public LoginResponse login(String loginId, String password) {
+    Optional<User> userOptional = userRepository.findByLoginId(loginId);
+    if (userOptional.isEmpty()) {
+      throw new AuthenticationFailedException("loginId または password が正しくありません");
+    }
+    return new LoginResponse("dummy-token", null);
+  }
 }
